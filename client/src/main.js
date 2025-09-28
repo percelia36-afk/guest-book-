40a273cb170010631977c62de7fa4ba45d74be2f
percelia36-afk <percelia36@gmail.com>
2025-09-28 22:35:31 +0100

    https

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,5 @@
 async function fetchGuests() {
-  const response = await fetch("http://localhost8080:/guests");
+  const response = await fetch("https://localhost:8080/guests");
   const jokes = await response.json();
   console.log(guests);
 }
