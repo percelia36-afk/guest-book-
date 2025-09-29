fcd6b8d7b827beec511863e800ab7c3b788c852a
percelia36-afk <percelia36@gmail.com>
2025-09-29 05:48:20 +0100

    last touches

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,41 @@ async function fetchGuests() {
   const response = await fetch("http://localhost:8080/guests");
   const guests = await response.json();
   console.log(guests);
+
+  guests.forEach((entry) => {
+    const div = document.createElement("div");
+    const pGuest = document.createElement("p");
+    const pComment = document.createElement("p");
+
+    pGuest.innerText = `Guest: ${entry.guest}`;
+    pComment.innerText = `Comment: ${entry.comment}`;
+
+    div.setAttribute("class", "guest-container");
+    div.append(pGuest, pComment);
+    document.body.appendChild(div);
+  });
 }
 
 fetchGuests();
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const beeInformation = new FormData(form);
+  const data = Object.fromEntries(beeInformation.entries());
+
+  console.log(data);
+
+  const responseFromAPI = await fetch("http://localhost:8080/guests", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await responseFromAPI.json();
+  console.log(result);
+});
--- sql.sql
+++ sql.sql
@@ -1,2 +1,12 @@
-'S Smith',
-    'Great views loads of way to spend you time and relax'
+CREATE TABLE guests (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  guest text,
+  comment text
+);
+INSERT INTO guests (guest,comment ) VALUES 
+( 'Bob' , 'Great amenities and helpfull staff'),
+('Robert' , 'Great fun to be had great food and great athomsphere' ),
+('Sam Addams' , 'Great  fun for the whole family' ),
+( 'S Smith', 'Great array of things to see and do');
+
+SELECT *FROM Guests
